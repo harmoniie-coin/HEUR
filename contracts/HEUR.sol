cf0f26011926bc011b12dd21ca269723df90c4e8
harry <harry@easyeuro.eu>
2025-05-29 14:43:02 +0800

    simplify access control

--- contracts/HEUR.sol
+++ contracts/HEUR.sol
@@ -2,12 +2,12 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {ERC20BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import {ERC20PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
 import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract HEUR is
@@ -15,16 +15,11 @@ contract HEUR is
     ERC20Upgradeable,
     ERC20BurnableUpgradeable,
     ERC20PausableUpgradeable,
-    AccessControlUpgradeable,
+    OwnableUpgradeable,
     ERC20PermitUpgradeable,
     UUPSUpgradeable
 {
     uint256 public constant VERSION = 1;
-
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BLACKLISTER_ROLE = keccak256("BLACKLISTER_ROLE");
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     mapping(address => bool) private _blacklist;
 
@@ -37,53 +32,43 @@ contract HEUR is
 
     uint8 private _decimals;
 
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
     function initialize(
         string memory name,
         string memory symbol,
         uint8 decimals_,
-        address defaultAdmin,
-        address pauser,
-        address minter,
-        address blacklister,
-        address upgrader
+        address initialOwner
     ) public initializer {
         require(decimals_ <= 18, "Invalid decimals");
-        require(defaultAdmin != address(0), "Invalid admin address");
-        require(pauser != address(0), "Invalid pauser address");
-        require(minter != address(0), "Invalid minter address");
-        require(blacklister != address(0), "Invalid blacklister address");
-        require(upgrader != address(0), "Invalid upgrader address");
 
         __ERC20_init(name, symbol);
         _decimals = decimals_;
         __ERC20Burnable_init();
         __ERC20Pausable_init();
-        __AccessControl_init();
+        __Ownable_init(initialOwner);
         __ERC20Permit_init(name);
         __UUPSUpgradeable_init();
-
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(PAUSER_ROLE, pauser);
-        _grantRole(MINTER_ROLE, minter);
-        _grantRole(BLACKLISTER_ROLE, blacklister);
-        _grantRole(UPGRADER_ROLE, upgrader);
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
+    function pause() public onlyOwner {
         _pause();
     }
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
+    function unpause() public onlyOwner {
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 amount) public onlyOwner {
         require(to != address(0), "Invalid recipient address");
         require(amount > 0, "Amount must be positive");
         _mint(to, amount);
     }
 
-    function blacklist(address account) external onlyRole(BLACKLISTER_ROLE) {
+    function blacklist(address account) external onlyOwner {
         require(account != address(0), "Invalid account address");
         require(account != msg.sender, "Cannot blacklist self");
         require(!_blacklist[account], "Account already blacklisted");
@@ -91,7 +76,7 @@ contract HEUR is
         emit Blacklisted(account);
     }
 
-    function unBlacklist(address account) external onlyRole(BLACKLISTER_ROLE) {
+    function unBlacklist(address account) external onlyOwner {
         require(account != address(0), "Invalid account address");
         require(_blacklist[account], "Account not blacklisted");
         _blacklist[account] = false;
@@ -102,9 +87,7 @@ contract HEUR is
         return _blacklist[account];
     }
 
-    function destroyBlacklistedFunds(
-        address account
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function destroyBlacklistedFunds(address account) public onlyOwner {
         require(_blacklist[account], "Account not blacklisted");
         uint256 dirtyFunds = balanceOf(account);
         require(dirtyFunds > 0, "Account has zero balance");
@@ -129,7 +112,7 @@ contract HEUR is
 
     function _authorizeUpgrade(
         address newImplementation
-    ) internal override onlyRole(UPGRADER_ROLE) {
+    ) internal override onlyOwner {
         require(
             newImplementation != address(0),
             "Invalid implementation address"
